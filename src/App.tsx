ca11fbde18d91d4776cc98e9344f4f1d1ae2231b
Mikołaj Mrózek <m.mik311@gmail.com>
2022-05-13 01:25:45 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import InfiniteScroll from './components/InfiniteScroll';
+import LaunchCard from './components/LaunchCard';
+import SearchBar from './components/SearchBar';
 
-function App() {
+const App: React.FC = () => {
+  const [querry, setQuerry] = useState<string>('')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="bg-gray-900 sticky top-0 py-4 my-2 mt-6">
+        <h1 className=" font-extrabold text-5xl py-6 mx-4 mb-2 px-4 text-transparent bg-clip-text bg-gradient-to-br from-blue-400 to-cyan-400">
+          SpaceX Launches
+        </h1>
+        <SearchBar setMainQuerry={setQuerry} />
+      </div>
+
+      <InfiniteScroll querry={querry} />
     </div>
   );
 }
